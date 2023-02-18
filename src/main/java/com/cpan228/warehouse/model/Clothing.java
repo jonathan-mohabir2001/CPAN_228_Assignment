0c83db1b921e671c9802c77005c84098be4230f8
Jonathan Mohabir <jonamohabir416@gmail.com>
2023-02-18 10:52:51 -0500

    Final commit, assignment completed Thymeleaf validations set.

--- src/main/java/com/cpan228/warehouse/model/Clothing.java
+++ src/main/java/com/cpan228/warehouse/model/Clothing.java
@@ -1,38 +1,23 @@
 package com.cpan228.warehouse.model;
-
 import lombok.Data;
-
-import java.util.Arrays;
-
 import lombok.Builder;
-
 @Builder
 @Data
 public class Clothing {
-
   private int id;
   private String name;
   private int yearProduced;
   private double price;
   private BrandingName brand;
 
-  public Clothing(int id, String name, int yearProduced, double price, BrandingName brand) {
-    if (price <= 1000) {
-      throw new IllegalArgumentException("Price must be more than 1000");
-    }
-    if (yearProduced <= 2021) {
-      throw new IllegalArgumentException("Year of production must be more than 2021");
-    }
-    if (!Arrays.asList(BrandingName.values()).contains(brand)) {
-      throw new IllegalArgumentException("Invalid brand name, must be one of: " + Arrays.asList(BrandingName.values()));
-    }
-    this.id = id;
-    this.name = name;
-    this.yearProduced = yearProduced;
-    this.price = price;
-    this.brand = brand;
+  public static Clothing createClothing(String name, int yearProduced, double price, BrandingName brand) {
+    return Clothing.builder()
+      .name(name)
+      .yearProduced(yearProduced)
+      .price(price)
+      .brand(brand)
+      .build();
   }
-
   public enum BrandingName {
     NIKE("Nike"),
     ADIDAS("Adidas"),
